c31c789c05763942366fccb98f641927a9ff5059
Megan Horsfield <meganthadev@gmail.com>
2020-03-09 02:52:56 +0000

    add methods to cli

--- lib/top_destinations/cli.rb
+++ lib/top_destinations/cli.rb
@@ -6,13 +6,57 @@ class TopDestinations::CLI
   end 
   
   def start 
-    get_dest_list
-      puts ""
-      puts "Which destinations would you like to see? 1-10 or 11-20?"
+    puts ""
+    puts "Which destinations would you like to see? 1-10 or 11-20?"
     input = gets.strip.to_i
-    get_user_list
     
+    print_dest(input)
+    
+    destination = TopDestinations::Destination.find(input.to_i)
+    
+    puts ""
+    puts "Which destination would you like to see interesting facts for?"
+    input = gets.strip
+    
+    print_dest(destination)
+    
+    puts ""
+    puts "Contact us for the best travel experience there is! Would you like to see a different destination? Please enter Y or N"
+
+    input = gets.strip.downcase
+    if input == "y"
+      start
+    elsif input == "n"
+      puts ""
+      puts "Contact us, we look forward to making your Banana Blammah traveling dreams come true!"
+      exit
+    else
+      puts ""
+      puts "Sorry, I don't understand that answer."
+      start
+    end
   end   
+  
+   def print_destinations(from_number)
+    puts ""
+    puts "---------- Destination #{from_number} - #{from_number+9} ----------"
+    puts ""
+    TopDestinations::Destination.all[from_number-1, 10].each.with_index(from_number) do |dest, index|
+      puts "#{index}. #{dest.name}"
+    end
+  end
+  
+  def print_dest(destination)
+    puts ""
+    puts "----------- #{destination.name} -----------"
+    puts ""
+    puts "Image Link:            #{destination.image_url}"
+    puts ""
+    puts "---------------Interesting Facts--------------"
+    puts ""
+    puts "Details:  #{destination.facts}"
+    puts ""
+  end
   
   def get_dest_list
     #will be scraped
@@ -20,10 +64,11 @@ class TopDestinations::CLI
   end 
   
   def get_user_list
-    @destinations.each_with_index do |index, dest| 
-     puts "#{index + 1} #{dest}"
+    chosen.list = gets.strip
+    @destinations.each_with_index(1) do |index, dest| 
+     puts "#{index} #{dest}"
       end
     end 
-  end   
+  
 
 end   
